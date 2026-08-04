38dd5441c33e5f0f1c903e0b64728187e3e69ac6
José Hermes <jose.hermes.dev@gmail.com>
2026-08-04 15:31:34 -0300

    adicionar: configurar servidor Express com dotenv e rota inicial

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,16 @@
-console.log("hello world");
+import dotenv from 'dotenv'
+import express, { Request, Response } from 'express';
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Olá do servidor TypeScript!');
+});
+
+app.listen({ port: parseInt(process.env.PORT || "3000") }, () => {
+  console.log(`Server is running on port ${process.env.PORT || 3000}`);
+});
