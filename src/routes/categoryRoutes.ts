cd25a034dc8dfe4b0b829e6b3040aac13c7fa597
José Hermes <jose.hermes.dev@gmail.com>
2026-08-08 16:35:10 -0300

    feat: adicionar rotas para gerenciamento de categorias com operações CRUD

---
+++ src/routes/categoryRoutes.ts
@@ -0,0 +1,11 @@
+import * as categoryController from '../controllers/categoryController';
+import { Router } from 'express';
+const router = Router();
+
+router.post('/categories', categoryController.createCategory);
+router.get('/categories', categoryController.getAllCategories);
+router.get('/categories/:id', categoryController.getCategoryById);
+router.put('/categories/:id', categoryController.updateCategory);
+router.delete('/categories/:id', categoryController.deleteCategory);
+
+export default router;
