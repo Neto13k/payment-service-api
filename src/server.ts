b1bea81956d7d71903d6411cebee7c8b9cdbbfae
José Hermes <jose.hermes.dev@gmail.com>
2026-08-12 19:04:15 -0300

    feat: adicionar rotas de produtos ao servidor

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import dotenv from 'dotenv'
 import express, { Request, Response } from 'express';
 import categoryRoutes from './routes/categoryRoutes';
 import customerRoutes from './routes/customerRoutes';
+import productRoutes from './routes/productRoutes';
 dotenv.config();
 
 const app = express();
@@ -9,6 +10,7 @@ app.use(express.json());
 
 app.use('/categories', categoryRoutes); // Adicionando as rotas de categorias
 app.use('/customers', customerRoutes); // Adicionando as rotas de clientes
+app.use('/products', productRoutes); // Adicionando as rotas de produtos
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Olá do servidor TypeScript!');
