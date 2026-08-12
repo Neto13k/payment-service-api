d1bd5112ea5dd9f96f1c0c83aaad2f36b8513f98
José Hermes <jose.hermes.dev@gmail.com>
2026-08-12 19:04:23 -0300

    feat: adicionar rotas de produtos com operações CRUD

---
+++ src/routes/productRoutes.ts
@@ -0,0 +1,11 @@
+import * as productController from '../controllers/productController'
+import { Router } from 'express';
+const router = Router();
+
+router.post('/', productController.createProduct);
+router.get('/', productController.getAllProducts);
+router.get('/:id', productController.getProductById);
+router.put('/:id', productController.updateProduct);
+router.delete('/:id', productController.deleteProduct);
+
+export default router;
