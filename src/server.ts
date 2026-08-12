0bf2d1df9423d83262816a39b7093b3fdb532912
José Hermes <jose.hermes.dev@gmail.com>
2026-08-12 20:26:37 -0300

    feat: adicionar rotas de cupons ao servidor

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import express, { Request, Response } from 'express';
 import categoryRoutes from './routes/categoryRoutes';
 import customerRoutes from './routes/customerRoutes';
 import productRoutes from './routes/productRoutes';
+import couponRoutes from './routes/couponRoutes';
 dotenv.config();
 
 const app = express();
@@ -11,6 +12,7 @@ app.use(express.json());
 app.use('/categories', categoryRoutes); // Adicionando as rotas de categorias
 app.use('/customers', customerRoutes); // Adicionando as rotas de clientes
 app.use('/products', productRoutes); // Adicionando as rotas de produtos
+app.use('/coupons', couponRoutes); // Adicionando as rotas de cupons
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Olá do servidor TypeScript!');
