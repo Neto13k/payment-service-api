201e0258df8b5db970f42fc04a7d770a2e9b3a56
José Hermes <jose.hermes.dev@gmail.com>
2026-08-14 20:38:28 -0300

    feat: adicionar rotas de pedidos ao servidor

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import categoryRoutes from './routes/categoryRoutes';
 import customerRoutes from './routes/customerRoutes';
 import productRoutes from './routes/productRoutes';
 import couponRoutes from './routes/couponRoutes';
+import orderRoutes from './routes/orderRoutes';
 dotenv.config();
 
 const app = express();
@@ -13,6 +14,7 @@ app.use('/categories', categoryRoutes); // Adicionando as rotas de categorias
 app.use('/customers', customerRoutes); // Adicionando as rotas de clientes
 app.use('/products', productRoutes); // Adicionando as rotas de produtos
 app.use('/coupons', couponRoutes); // Adicionando as rotas de cupons
+app.use('/orders', orderRoutes); // Adicionando as rotas de pedidos
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Olá do servidor TypeScript!');
